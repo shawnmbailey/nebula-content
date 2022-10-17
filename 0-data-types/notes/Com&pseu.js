43c2ceecb72267d9f2f57b634841e340ed411053
shawnmbailey <you@example.com>
2022-10-17 18:00:16 -0400

    complete assignment

---
+++ 0-data-types/notes/Com&pseu.js
@@ -0,0 +1,61 @@
+// This is a comment
+// This is a comment 
+// const iNoLongerNeedThis =3;
+
+/*
+This a comment 
+This is a comment 
+// const iNoLongerNeedThis =3;
+*/
+
+// psuedo-coding 
+/*
+function functionName (parameters){
+Output or
+"Return Statement"
+} 
+
+RepeatString Function
+
+Write a function that accepts 
+an interger (n) and string of (s) as
+parameters, and returns a string
+of (s) repeated exactly (n) times. 
+
+*/
+
+const ingredient1 = 'garlic powder'
+
+// Primitives 
+' string' let carName2 = 'Volvo XC60'; 
+null const thisIsmyVariable = null
+undefined let car;  
+integer example let x2 = 34;   
+Boolean 
+true or false
+truthy 
+falsey
+// Refrence 
+object{} const person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
+Array[] const cars = ["Saab", "Volvo", "BMW"];
+
+// Variables
+
+Const 
+ Constant Never Changes cannot re-assign
+
+ Let
+
+Let it be  Can be Re-Assigned
+Const and Let are Block Scope 
+
+// Block Scope
+
+
+Var = no longer used 
+
+function repeatHello3Times() {
+    var s = hello 
+    return s * 3
+ }
+
---
+++ 1-variables/assignments/1-variable-hw.js
@@ -0,0 +1,38 @@
+// Create a .js file that satisfies assignment below 
+// Copy the final assignment text into this assignment to submit
+// For each exercise you'll want to log the result to the console to check your work
+
+
+//1. Create a set of variables that define an animal (age, height, name, etc...). Must be at least 6 variables.
+
+const Name = 'Rex'
+const Species = 'Dog'
+const Age = 4
+const Breed = 'Cane Corso'
+const Color = 'Black'
+const Height = '28 in'
+const animal = `${Species}, ${Name}
+ ${Age}, ${Breed}
+ ${Color}, ${Height}`
+
+ console.log(animal)
+
+
+
+
+
+
+
+ //2. Create a set of variables that define a vehicle. Must be at least 6 variables.
+
+ const Make = 'Bmw'
+ const Model = 'M6'
+ const color = 'Burgundy'
+ const Interior = 'Leather'
+ const Headlights = 'Halo'
+ const Body = 'Coupe'
+ const car = `${Make} , ${Model}
+ ${color}, ${Interior}
+ ${Headlights}, ${Body}`
+
+console.log(car)
