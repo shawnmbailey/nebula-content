d596a2e4f6cf4beab3f7fce11f09f8fcff41eee7
shawnmbailey <you@example.com>
2022-10-19 23:47:13 -0400

    .

---
+++ 2-numbers/assignments/1-numbers-homework.js
@@ -0,0 +1,69 @@
+
+// Remeber to console.log() to debug your code and check your solutions!
+
+
+// Declare a variable and assign it to a numeric value
+let num1= 5; 
+console.log(num1);
+// Half that value and re-assign it to the previous variable 
+answer = num1/2;
+console.log(answer);
+
+
+// Create two negative numbers, assign them to variables and subtract one from the other together 
+const neg1 = -5;
+const neg2 = -12;
+sub= neg1 - neg2;
+console.log(sub);
+
+
+// Create three numbers, assign them to variables and multiply them together 
+const g = 3;
+const h = 5;
+const i = 8;
+multi= g * h * i;
+console.log(multi);
+
+
+
+// Create two even numbers, assign them to variables and divide one from the other 
+const evennum1 =2;
+const evennum2 = 8;
+deven = evennum1 / evennum2;
+console.log(deven);
+// Create two odd numbers, assign them to variables and find the remainder of dividing one by the other 
+const oddnum1 = 3;
+const oddnum2 = 5;
+dodd = oddnum1 / oddnum2;
+console.log(dodd);
+
+// find the minimum value in a set of numbers: 10,20,30,40,50 
+console.log(Math.min(10,20,30,40,50) );
+
+
+// find the maximum value in a set of numbers: -1,-2,-3,-4,-5 
+console.log(Math.max(-1,-2,-3,-4,-5 ));
+
+// Create a variable and find it's remainder, if divided by 2
+let num = 11
+remainder = num % 2;
+console.log(remainder);
+
+
+// find the square root of 144 
+console.log(Math.sqrt(144));
+
+
+// find the value of 3 to the power of 7 
+console.log(Math.pow(3, 7)); 
+
+// find the value of 73.2 rounded 
+console.log(Math.ceil(73.2));
+
+
+// find the rounded-down value of 9.9 
+console.log(Math.floor(9.9));
+
+
+// find the absolute value of -1231
+console.log(Math.abs(-1231));
